b47128402def08414c7996ae48e0727f83b56794
Marina Aglic <magliccuv@pmfst.hr>
2021-09-29 17:42:14 +0200

    commit

---
+++ OTTER/BGL.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace OTTER
+{
+    partial class BGL
+    {
+
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.syncRate = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // syncRate
+            // 
+            this.syncRate.AutoSize = true;
+            this.syncRate.BackColor = System.Drawing.Color.Transparent;
+            this.syncRate.Location = new System.Drawing.Point(12, 394);
+            this.syncRate.Name = "syncRate";
+            this.syncRate.Size = new System.Drawing.Size(71, 52);
+            this.syncRate.TabIndex = 0;
+            this.syncRate.Text = "60";
+            this.syncRate.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 17;
+            this.timer1.Tick += new System.EventHandler(this.Update);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 250;
+            this.timer2.Tick += new System.EventHandler(this.updateFrameRate);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightPink;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Gray;
+            this.button1.Location = new System.Drawing.Point(246, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.button1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button1_KeyUp);
+            // 
+            // BGL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(26F, 52F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(700, 498);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.syncRate);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+            this.MaximizeBox = false;
+            this.Name = "BGL";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "2DGL";
+            this.Load += new System.EventHandler(this.startTimer);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Draw);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mouseClicked);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label syncRate;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ OTTER/Likovi.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OTTER
+{
+    public abstract class Likovi : Sprite
+    {
+        protected int brzina;
+        public int Brzina
+        {
+            get { return brzina; }
+            set { brzina = value; }
+        }
+        public Likovi(string slika, int xcor, int ycor, int b)
+            :base(slika, xcor, ycor)
+        {
+            this.brzina = b;
+        }
+    }
+
+    public class Riba : Likovi
+    {
+
+        public Riba(string slika, int xcor, int ycor)
+            :base(slika,xcor,ycor, 0)
+        {
+            
+        }
+
+        public override int X
+        {
+            get { return x; }
+            set
+            {
+                if (!(value < GameOptions.LeftEdge) && !(value > GameOptions.RightEdge - this.Width))
+                    x = value;
+
+            }
+        }
+
+        public override int Y
+        {
+            get { return y; }
+            set
+            {
+                if (!(value < GameOptions.UpEdge) && !(value > GameOptions.DownEdge - this.Width))
+                    y = value;
+            }
+        }
+    }
+
+    public class MrtvaRiba : Likovi
+    {
+        public MrtvaRiba(string slika, int xcor, int ycor,int b)
+            :base(slika,xcor,ycor, 0)
+        {
+            this.brzina = b;
+        }
+
+        public override int X
+        {
+            get { return x; }
+            set
+            {
+                if (!(value < GameOptions.LeftEdge) && !(value > GameOptions.RightEdge - this.Width))
+                    x = value;
+
+            }
+        }
+
+       
+    }
+
+    public class Hobotnica : Likovi
+    {
+        public Hobotnica(string slika, int xcor, int ycor)
+            :base(slika,xcor,ycor, 0)
+        {
+            
+        }
+
+        public override int X
+        {
+            get { return x; }
+            set
+            {
+                if (!(value < GameOptions.LeftEdge) && !(value > GameOptions.RightEdge - this.Width))
+                    x = value;
+
+            }
+        }
+
+        public override int Y
+        {
+            get { return y; }
+            set
+            {
+                if (!(value < GameOptions.UpEdge) && !(value > GameOptions.DownEdge - this.Width))
+                    y = value;
+            }
+        }
+    }
+
+    public class Pingvin : Likovi
+    {
+        protected int bodovi;
+        protected int life;
+        public int Bodovi
+        {
+            get { return bodovi; }
+            set { bodovi = value; }
+        }
+        public int Life
+        {
+            get { return life; }
+            set
+            {
+                if(value<=0)
+                {
+                    throw new ArgumentException();
+                }
+                life = value;
+            }
+        }
+        public Pingvin(string slika, int xcor, int ycor, int bod, int li)
+            :base(slika,xcor, ycor, 5)
+        {
+            this.bodovi = bod;
+            this.life = li;
+        }
+    }
+
+    public class Zvijezda :Likovi
+    {
+        public Zvijezda(string slika, int xcor, int ycor,int b)
+            :base(slika,xcor, ycor,0)
+        {
+            this.brzina = b;
+        }
+        public override int X
+        {
+            get { return x; }
+            set
+            {
+                if (!(value < GameOptions.LeftEdge) && !(value > GameOptions.RightEdge - this.Width))
+                    x = value;
+
+            }
+        }
+
+       
+    }
+}
